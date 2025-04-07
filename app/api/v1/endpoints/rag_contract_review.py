23c3045f2fcea82fb16a82d14b65754ac93c5f7e
sc2bpldw <sc2bpldw@gmail.com>
2025-04-07 18:18:14 +0800

    fix: debug

--- app/api/v1/endpoints/rag_contract_review.py
+++ app/api/v1/endpoints/rag_contract_review.py
@@ -2,6 +2,7 @@ import os
 from http import HTTPStatus
 from dashscope import Application
 from fastapi import APIRouter, Depends, HTTPException, Query
+from fastapi.responses import StreamingResponse
 from app.schemas.rag_contract_review import RagContractReviewRequest, RagContractReviewResponse
 
 router = APIRouter()
@@ -16,23 +17,24 @@ pipeline_ids = ["xedc4orobz"]
 def rag_contract_review(user_prompt: str = Query(...)):
     
   response = Application.call(
-      api_key=contract_api_key, 
-      app_id=app_id,
-      prompt=user_prompt,
-      rag_options={
-          "pipeline_ids": pipeline_ids,
-      }
+    api_key=contract_api_key, 
+    app_id=app_id,
+    prompt=user_prompt,
+    rag_options={
+        "pipeline_ids": pipeline_ids,
+    },
+    stream=True
   )
 
   if response.status_code != HTTPStatus.OK:
-      print(f'request_id={response.request_id}')
-      print(f'code={response.status_code}')
-      print(f'message={response.message}')
-      print(f'请参考文档：https://help.aliyun.com/zh/model-studio/developer-reference/error-code')
+    print(f'request_id={response.request_id}')
+    print(f'code={response.status_code}')
+    print(f'message={response.message}')
+    print(f'请参考文档：https://help.aliyun.com/zh/model-studio/developer-reference/error-code')
   else:
-      print('%s\n' % (response.output.text))  # 处理只输出文本text
-      print('%s\n' % (response.usage)) # {"models": [{"model_id": "qwen-max-latest", "input_tokens": 6164, "output_tokens": 635}]}
+    print('%s\n' % (response.output.text))  # 处理只输出文本text
+    print('%s\n' % (response.usage)) # {"models": [{"model_id": "qwen-max-latest", "input_tokens": 6164, "output_tokens": 635}]}
             
   return RagContractReviewResponse(
-     text=f"{response.output.text}"
+    text=f"{response.output.text}"
   )
